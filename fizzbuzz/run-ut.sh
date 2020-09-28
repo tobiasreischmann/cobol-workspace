50bae8caf3d3958a29febb445b2194c59508f9d7
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2020-09-28 13:23:38 +0200

    Added fizzbuzz solution

--- fizzbuzz/run-ut.sh
+++ fizzbuzz/run-ut.sh
@@ -1,3 +1,1 @@
-/home/cobol/cobol-unit-test/run-ut -d -p fizzbuzz FIZZBUZC FIZZBUZZ FIZZBUZT
-
-#cobc -x -std=ibm /vagrant/cobol/fizzbuzz/src/FIZZBUZZ.CBL -o /vagrant/cobol/fizzbuzz/target/FIZZBUZZ
+/home/cobol/cobol-unit-test/run-ut -p fizzbuzz FIZZBUZZ_C FIZZBUZZ_M FIZZBUZZ_T 
